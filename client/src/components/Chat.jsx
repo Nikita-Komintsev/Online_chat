3ed45cbf06d7710b753ee9f964463898012befa2
Nikita-Komintsev <nikita.comin@gmail.com>
2023-08-06 19:26:16 +0300

    update chat component

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import { useLocation } from 'react-router-dom';
 
+import styles from '../styles/Chat.module.css';
+
 const socket = io.connect('http://localhost:5000');
 
 const Chat = () => {
   const {search} = useLocation();
-  const [params, setParams] = useState(null);
+  const [params, setParams] = useState({ room: "", user: "" });
   const [state, setState] = useState([]);
+  const [message, setMessage] = useState("");
 
   useEffect(()=>{
     const searchParams = Object.fromEntries(new URLSearchParams(search));
@@ -21,8 +24,47 @@ const Chat = () => {
     });
   }, [])
 
+  const leftRoom = () => {};
+  const handleChange = () => {};
+  const handleSubmit = () => {};
+
   return (
-    <div>Chat</div>
+    <div className={styles.wrap}>
+      <div className={styles.header}>
+        <div className={styles.title}>
+          {params.room}
+        </div>
+        <div className={styles.users}>
+          0 users in room
+        </div>
+        <button className={styles.left} onClick={leftRoom}>
+          Left room
+        </button>
+      </div>
+
+      <div className={styles.messages}>
+          {state.map(({message}, idx) => (
+            <span key={idx} >{message}</span>))}
+        </div>
+
+        <form className={styles.form}>
+          <div className={styles.input}>
+            <input 
+              type="text" 
+              name='message' 
+              value={message} 
+              placeholder='Write message'
+              onChange={handleChange}
+              autoComplete='off'
+              required
+            />
+          </div>
+
+          <div className={styles.button}> 
+            <input type='submit' onSubmit={handleSubmit} value="Send message" />
+          </div>
+        </form>
+    </div>
   )
 }
 
